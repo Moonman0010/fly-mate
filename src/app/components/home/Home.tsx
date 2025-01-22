b54af8cc8fa76bda48d7539b3068a98ce1fb3eb4
shubhanshu dev puri <sdevpuri@gmail.com>
2025-01-22 17:52:12 -0600

    about component initial commit

--- src/app/components/home/Home.tsx
+++ src/app/components/home/Home.tsx
@@ -1,6 +1,7 @@
 import Header from "./header";
 import Tagline from "./tagline";
 import FlyMatePoster from "./poster";
+import About from"./about";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Header />
       <Tagline />
       <FlyMatePoster />
+      <About/>
     </div>
   );
 }
---
+++ src/app/components/home/about.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+const About = () => {
+    return (
+        <div >
+          this is about section comming soon
+        </div>
+      );
+}
+export default About;
